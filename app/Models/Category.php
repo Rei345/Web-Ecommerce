07703ab20e42d1209fb3e1111567028008ef95c8
Reinhard Marcelino Sitompul <reinhardsitompul03@gmail.com>
2025-02-11 19:30:34 +0700

    Model & Migration Configuration

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'slug', 'icon'];
+
+    public function shoes(): HasMany
+    {
+        return $this->hasMany(Shoe::class);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
